1f1be5125490debf65cc5275f0e4357206c0252f
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-11-08 16:39:54 -0500

    joke app

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import styles from './styles';
+import Joke from './components/Jokes';
+import Punchline from './components/Punchline';
+import Another from './components/Another';
 
 export default function App() {
+
+  //create state to hold jokes
+  const [jokeList, setJokeList] = useState([]);
+
+  //state to hold last joke index
+  const [currentJokeIndex, setCurrentJokeIndex] = useState(0);
+
+  //create function to get another
+  const getNextJoke = () => {
+    if (currentJokeIndex < jokeList.length - 1) {
+      setCurrentJokeIndex(currentJokeIndex + 1);
+    } else {
+      setCurrentJokeIndex(0);
+    }
+  }
+
+  //useEffect to get all jokes
+  useEffect(() => {
+    fetch('https://api.sampleapis.com/jokes/goodJokes')
+      .then(res => res.json())
+      .then(setJokeList)
+      .catch(alert)
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {
+        jokeList
+          ? <>
+            <Joke joke={jokeList[currentJokeIndex].setup} />
+            <Punchline punchline={jokeList[currentJokeIndex].punchline} />
+          </>
+          : null
+      }
+      <Another getNextJoke={getNextJoke}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Jokes.jsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import styles from '../styles';
+
+export default function Joke({joke}){
+    return (
+        <>
+            <Text style={styles.jokeText}>{joke}</Text>
+        </>
+    )
+}
